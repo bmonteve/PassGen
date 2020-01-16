74bd19ac771905414de541a71ba3c8c2fca6b741
Brian Monteverde <bmonteve@gmail.com>
2020-01-15 22:30:05 -0800

    Fixed improper user inputs and added code comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PassGen
+
+1) Purpose: Allows a user to generate a random password of a desired specified length and character types. 
+
+2) Functionalilty: Displays the password generator immediately. Allows the user to hit a generate password button. It then prompts the user to designate a length for the password and charcter types for the password. It prevents the user from entering values that are not within 8 and 128 for character length unless the user cancels their generate button click. It also forces the user to choose at least one character type for the password. The copy button places the generated password in the users operating clipboard. It is also responsive to different viewport sizes. 
+
+![password generator demo](./ScreenShot2020-01-15at10.25.39PM.png)
--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//Three DOM elements to handle the two buttons and password textarea
 var generate = document.querySelector("#generate");
 var copy = document.querySelector("#copy");
 var passText = document.querySelector("#password");
 
+//User input, possible characters, and password creator variables
 var passLength;
-
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var number = "0123456789";
@@ -13,31 +14,51 @@ var print = "";
 var selector;
 
 
-
+//Creates a password based on user input when the generate button is clicked
 function createPassword(){
     
+    //clears out the text area when the button is clicked
     clearTextArea();
 
+    /* Handles multiple user input types and continues to run until either cancel is pressed or 
+     an input between 8 and 128 is entered*/
     while (passLength < 8 || passLength > 128){
         passLength = prompt("Enter the length of your desired password. (8-128 characters)");
+        
+        //creates a number from the user input
         if(passLength != null)
             passLength = parseInt (passLength);
+
+        //handles the cancel button to break out of loop 
         else if (passLength == null)
             break;
-        else if (isNaN(passLength)){
+        
+        //handles if the user enters no input or a characer that isn't a number
+        if (isNaN(passLength)){
             passLength = 0;
         }
     }
 
-    console.log(passLength);
-    
+    /* Handles if the user input is an acceptable number and cancel not pressed.
+    Processes the type of characters the user would like in the password*/ 
     if (passLength != null){
         var specType = confirm("Would you like your password to include special characters?");
         var numType =  confirm("Would you like your password to include numeric characters?");
         var lowType =  confirm("Would you like your password to include lowercase characters?");
         var upType =  confirm("Would you like your password to include uppercase characters?");
     }
+
+    //handles the possibility the user does not pick at least one character type
+    while (!specType && !numType && !lowType && !upType && passLength != null){
+        alert("No character types were selected. Try again.")
+        var specType = confirm("Would you like your password to include special characters?");
+        var numType =  confirm("Would you like your password to include numeric characters?");
+        var lowType =  confirm("Would you like your password to include lowercase characters?");
+        var upType =  confirm("Would you like your password to include uppercase characters?");
+    }
     
+    /*creates an array with the possible characters for the password based on the user's selected
+    character types */
     if(specType == true)
         password = password.concat(special.split(""));
     if (numType == true)
@@ -47,6 +68,7 @@ function createPassword(){
     if (upType == true)
         password = password.concat(upper.split(""));
 
+    //randomly creates a string based on the character types chosen by the user    
     for (index = 0; index < passLength; index++){
         selector = Math.floor(Math.random()*password.length);
         print += password[selector];
@@ -56,7 +78,8 @@ function createPassword(){
     
 }
 
-
+/*resets the textarea value, the random password created, the users password length, 
+and possible character options*/
 function clearTextArea() {
     passText.value = " ";
     print = "";
@@ -64,11 +87,13 @@ function clearTextArea() {
     password = [];
 }
 
+//copies the textarea to the users clipboard
 function copyToClip(){
     passText.select();
     document.execCommand("copy");
 }
 
+//handles clicks on the two buttons
 generate.addEventListener("click", createPassword);
 copy.addEventListener("click", copyToClip);
 
